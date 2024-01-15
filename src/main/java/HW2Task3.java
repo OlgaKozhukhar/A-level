b8ac0d900de14f3a87c9ea3f65502edf9af0dc01
OlgaKozhukhar <soi198607@gmail.com>
2024-01-15 20:19:15 +0200

    add initial implementation

---
+++ src/main/java/HW2Task3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HW2Task3 {
+
+    public static void main(String[] args) {
+
+        // Задание 3: Определить четность числа
+        System.out.println("\nЗадание 3:");
+        checkEvenOdd();
+    }
+
+    // Задание 3: Определить четность числа
+    private static void checkEvenOdd() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Число " + number + " четное.");
+        } else {
+            System.out.println("Число " + number + " нечетное.");
+        }
+    }
+}
