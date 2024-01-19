3fa1966970a9ab2042a0b16d0eb3cc027fc11cae
OlgaKozhukhar <soi198607@gmail.com>
2024-01-19 17:41:34 +0200

    add initial implementation

--- src/main/java/ua/kozhukhar/hw3/Task2.java
+++ src/main/java/ua/kozhukhar/hw3/Task2.java
@@ -2,7 +2,7 @@ package ua.kozhukhar.hw3;
 
 import java.util.Scanner;
 
-public class HW3Task2 {
+public class Task2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
