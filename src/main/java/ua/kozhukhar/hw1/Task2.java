1298cbbf8cd3a574f24130d73f62761e447e89c3
OlgaKozhukhar <soi198607@gmail.com>
2024-01-19 17:40:30 +0200

    add initial implementation

---
+++ src/main/java/ua/kozhukhar/hw1/Task2.java
@@ -0,0 +1,11 @@
+package ua.kozhukhar.hw1;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        System.out.println("Name = Olga");
+        int height = 173;
+        double weight = 67.5;
+        System.out.println("Name=Olga" + "Height=" + height + "Weight=" + weight);
+    }
+}
