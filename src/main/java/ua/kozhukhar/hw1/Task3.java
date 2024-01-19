17c93119c88e77d5aadf5978faba2190d1cbe559
OlgaKozhukhar <soi198607@gmail.com>
2024-01-19 17:42:55 +0200

    add initial implementation

---
+++ src/main/java/ua/kozhukhar/hw1/Task3.java
@@ -0,0 +1,14 @@
+package ua.kozhukhar.hw1;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println (a);
+        System.out.println (b);
+    }
+}
